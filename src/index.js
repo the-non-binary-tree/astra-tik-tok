5e97005c8c506eb414113d9f1be53a416f513110
Tik Tok <TikTok@example.com>
2021-10-13 21:41:35 +0000

    Adding new functions

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { HashRouter, Route , Switch} from 'react-router-dom'
+import Home from './pages/Home'
+import Upload from './pages/Upload'
+import Header from './components/Header'
+import "core-js/stable"
+import "regenerator-runtime/runtime"
+import './App.css'
+
+const App = () => {
+  
+  return (
+    <HashRouter>
+      <Header />
+    <Switch>
+      <Route path= "/upload" component={Upload}/>
+      <Route path= "/" component={Home}/>
+    </Switch>
+  </HashRouter>
+  )
+}
+
+export default App
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
